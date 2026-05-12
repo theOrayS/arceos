a758ab57702d755fb7d490ec1b69bb1c1de9b421
wangzhengzhuo <wangzhengzhuo@localhost>
2026-05-13 01:24:56 +0800

    refactor: localize signal abi helpers

--- examples/shell/src/uspace.rs
+++ examples/shell/src/uspace.rs
@@ -78,8 +78,8 @@ use runtime_paths::{
 use select_fdset::{SelectMode, poll_fd_set, read_fd_set, read_pselect_deadline, write_fd_set};
 #[cfg(target_arch = "riscv64")]
 use signal_abi::{
-    RiscvKernelSigset, RiscvSignalFpState, RiscvSignalFrame, RiscvSignalInfo,
-    RiscvSignalSigcontext, RiscvSignalStack, RiscvSignalUcontext,
+    RiscvSignalFrame, apply_riscv_sigcontext, make_riscv_signal_frame, riscv_signal_frame_offsets,
+    riscv_signal_frame_size, trap_frame_to_riscv_sigcontext,
 };
 use synthetic_fs::{
     dev_shm_host_path, ensure_dev_shm_dir, is_proc_self_maps_path, proc_self_maps_fd_entry,
@@ -983,97 +983,6 @@ fn terminate_current_thread(process: &UserProcess, code: i32) -> ! {
 }
 
 #[cfg(target_arch = "riscv64")]
-fn trap_frame_to_riscv_sigcontext(tf: &TrapFrame) -> RiscvSignalSigcontext {
-    RiscvSignalSigcontext {
-        gregs: [
-            tf.sepc,
-            tf.regs.ra,
-            tf.regs.sp,
-            tf.regs.gp,
-            tf.regs.tp,
-            tf.regs.t0,
-            tf.regs.t1,
-            tf.regs.t2,
-            tf.regs.s0,
-            tf.regs.s1,
-            tf.regs.a0,
-            tf.regs.a1,
-            tf.regs.a2,
-            tf.regs.a3,
-            tf.regs.a4,
-            tf.regs.a5,
-            tf.regs.a6,
-            tf.regs.a7,
-            tf.regs.s2,
-            tf.regs.s3,
-            tf.regs.s4,
-            tf.regs.s5,
-            tf.regs.s6,
-            tf.regs.s7,
-            tf.regs.s8,
-            tf.regs.s9,
-            tf.regs.s10,
-            tf.regs.s11,
-            tf.regs.t3,
-            tf.regs.t4,
-            tf.regs.t5,
-            tf.regs.t6,
-        ],
-        fpstate: RiscvSignalFpState {
-            bytes: [0; RISCV_SIGNAL_FPSTATE_BYTES],
-        },
-    }
-}
-
-#[cfg(target_arch = "riscv64")]
-fn apply_riscv_sigcontext(tf: &mut TrapFrame, sigcontext: &RiscvSignalSigcontext) {
-    tf.sepc = sigcontext.gregs[0];
-    tf.regs.zero = 0;
-    tf.regs.ra = sigcontext.gregs[1];
-    tf.regs.sp = sigcontext.gregs[2];
-    tf.regs.gp = sigcontext.gregs[3];
-    tf.regs.tp = sigcontext.gregs[4];
-    tf.regs.t0 = sigcontext.gregs[5];
-    tf.regs.t1 = sigcontext.gregs[6];
-    tf.regs.t2 = sigcontext.gregs[7];
-    tf.regs.s0 = sigcontext.gregs[8];
-    tf.regs.s1 = sigcontext.gregs[9];
-    tf.regs.a0 = sigcontext.gregs[10];
-    tf.regs.a1 = sigcontext.gregs[11];
-    tf.regs.a2 = sigcontext.gregs[12];
-    tf.regs.a3 = sigcontext.gregs[13];
-    tf.regs.a4 = sigcontext.gregs[14];
-    tf.regs.a5 = sigcontext.gregs[15];
-    tf.regs.a6 = sigcontext.gregs[16];
-    tf.regs.a7 = sigcontext.gregs[17];
-    tf.regs.s2 = sigcontext.gregs[18];
-    tf.regs.s3 = sigcontext.gregs[19];
-    tf.regs.s4 = sigcontext.gregs[20];
-    tf.regs.s5 = sigcontext.gregs[21];
-    tf.regs.s6 = sigcontext.gregs[22];
-    tf.regs.s7 = sigcontext.gregs[23];
-    tf.regs.s8 = sigcontext.gregs[24];
-    tf.regs.s9 = sigcontext.gregs[25];
-    tf.regs.s10 = sigcontext.gregs[26];
-    tf.regs.s11 = sigcontext.gregs[27];
-    tf.regs.t3 = sigcontext.gregs[28];
-    tf.regs.t4 = sigcontext.gregs[29];
-    tf.regs.t5 = sigcontext.gregs[30];
-    tf.regs.t6 = sigcontext.gregs[31];
-}
-
-#[cfg(target_arch = "riscv64")]
-fn make_riscv_siginfo(sig: i32, code: i32, tid: i32) -> RiscvSignalInfo {
-    let mut info = RiscvSignalInfo { bytes: [0; 128] };
-    info.bytes[0..4].copy_from_slice(&sig.to_ne_bytes());
-    info.bytes[4..8].copy_from_slice(&0i32.to_ne_bytes());
-    info.bytes[8..12].copy_from_slice(&code.to_ne_bytes());
-    info.bytes[16..20].copy_from_slice(&tid.to_ne_bytes());
-    info.bytes[20..24].copy_from_slice(&0u32.to_ne_bytes());
-    info
-}
-
-#[cfg(target_arch = "riscv64")]
 fn ensure_signal_frame_pages(
     process: &UserProcess,
     start: usize,
@@ -1130,29 +1039,19 @@ fn inject_pending_signal(
         return Ok(());
     }
     let current_mask = ext.signal_mask.load(Ordering::Acquire);
-    let frame_size = size_of::<RiscvSignalFrame>();
+    let frame_size = riscv_signal_frame_size();
     let frame_addr = align_down(tf.regs.sp.saturating_sub(frame_size), 16);
     ensure_signal_frame_pages(ext.process.as_ref(), frame_addr, frame_size)?;
 
-    let frame = RiscvSignalFrame {
-        info: make_riscv_siginfo(sig, SI_TKILL_CODE, current_tid()),
-        ucontext: RiscvSignalUcontext {
-            flags: 0,
-            link: 0,
-            stack: RiscvSignalStack {
-                sp: 0,
-                stack_flags: SS_DISABLE,
-                stack_pad: 0,
-                size: 0,
-            },
-            sigmask: RiscvKernelSigset {
-                sig: [current_mask],
-                reserved: [0; RISCV_SIGNAL_SIGSET_RESERVED_BYTES],
-            },
-            mcontext: trap_frame_to_riscv_sigcontext(tf),
-        },
-        trampoline: RISCV_SIGTRAMP_CODE,
-    };
+    let frame = make_riscv_signal_frame(
+        sig,
+        SI_TKILL_CODE,
+        current_tid(),
+        current_mask,
+        SS_DISABLE,
+        RISCV_SIGTRAMP_CODE,
+        trap_frame_to_riscv_sigcontext(tf),
+    );
 
     let frame_ret = write_user_value(ext.process.as_ref(), frame_addr, &frame);
     if frame_ret != 0 {
@@ -1174,11 +1073,12 @@ fn inject_pending_signal(
         );
     }
 
+    let frame_offsets = riscv_signal_frame_offsets();
     tf.regs.sp = frame_addr;
-    tf.regs.ra = frame_addr + offset_of!(RiscvSignalFrame, trampoline);
+    tf.regs.ra = frame_addr + frame_offsets.trampoline;
     tf.regs.a0 = sig as usize;
-    tf.regs.a1 = frame_addr + offset_of!(RiscvSignalFrame, info);
-    tf.regs.a2 = frame_addr + offset_of!(RiscvSignalFrame, ucontext);
+    tf.regs.a1 = frame_addr + frame_offsets.info;
+    tf.regs.a2 = frame_addr + frame_offsets.ucontext;
     tf.sepc = handler;
     Ok(())
 }
--- examples/shell/src/uspace/signal_abi.rs
+++ examples/shell/src/uspace/signal_abi.rs
@@ -1,5 +1,8 @@
 #[cfg(target_arch = "riscv64")]
-use core::mem::size_of;
+use core::mem::{offset_of, size_of};
+
+#[cfg(target_arch = "riscv64")]
+use axhal::context::TrapFrame;
 
 #[cfg(target_arch = "riscv64")]
 use super::linux_abi::{RISCV_SIGNAL_FPSTATE_BYTES, RISCV_SIGNAL_SIGSET_RESERVED_BYTES};
@@ -62,6 +65,149 @@ pub(super) struct RiscvSignalFrame {
     pub(super) info: RiscvSignalInfo,
     pub(super) ucontext: RiscvSignalUcontext,
     pub(super) trampoline: [u32; 3],
+}
+
+#[cfg(target_arch = "riscv64")]
+pub(super) struct RiscvSignalFrameOffsets {
+    pub(super) info: usize,
+    pub(super) ucontext: usize,
+    pub(super) trampoline: usize,
+}
+
+#[cfg(target_arch = "riscv64")]
+pub(super) fn riscv_signal_frame_size() -> usize {
+    size_of::<RiscvSignalFrame>()
+}
+
+#[cfg(target_arch = "riscv64")]
+pub(super) fn riscv_signal_frame_offsets() -> RiscvSignalFrameOffsets {
+    RiscvSignalFrameOffsets {
+        info: offset_of!(RiscvSignalFrame, info),
+        ucontext: offset_of!(RiscvSignalFrame, ucontext),
+        trampoline: offset_of!(RiscvSignalFrame, trampoline),
+    }
+}
+
+#[cfg(target_arch = "riscv64")]
+pub(super) fn trap_frame_to_riscv_sigcontext(tf: &TrapFrame) -> RiscvSignalSigcontext {
+    RiscvSignalSigcontext {
+        gregs: [
+            tf.sepc,
+            tf.regs.ra,
+            tf.regs.sp,
+            tf.regs.gp,
+            tf.regs.tp,
+            tf.regs.t0,
+            tf.regs.t1,
+            tf.regs.t2,
+            tf.regs.s0,
+            tf.regs.s1,
+            tf.regs.a0,
+            tf.regs.a1,
+            tf.regs.a2,
+            tf.regs.a3,
+            tf.regs.a4,
+            tf.regs.a5,
+            tf.regs.a6,
+            tf.regs.a7,
+            tf.regs.s2,
+            tf.regs.s3,
+            tf.regs.s4,
+            tf.regs.s5,
+            tf.regs.s6,
+            tf.regs.s7,
+            tf.regs.s8,
+            tf.regs.s9,
+            tf.regs.s10,
+            tf.regs.s11,
+            tf.regs.t3,
+            tf.regs.t4,
+            tf.regs.t5,
+            tf.regs.t6,
+        ],
+        fpstate: RiscvSignalFpState {
+            bytes: [0; RISCV_SIGNAL_FPSTATE_BYTES],
+        },
+    }
+}
+
+#[cfg(target_arch = "riscv64")]
+pub(super) fn apply_riscv_sigcontext(tf: &mut TrapFrame, sigcontext: &RiscvSignalSigcontext) {
+    tf.sepc = sigcontext.gregs[0];
+    tf.regs.zero = 0;
+    tf.regs.ra = sigcontext.gregs[1];
+    tf.regs.sp = sigcontext.gregs[2];
+    tf.regs.gp = sigcontext.gregs[3];
+    tf.regs.tp = sigcontext.gregs[4];
+    tf.regs.t0 = sigcontext.gregs[5];
+    tf.regs.t1 = sigcontext.gregs[6];
+    tf.regs.t2 = sigcontext.gregs[7];
+    tf.regs.s0 = sigcontext.gregs[8];
+    tf.regs.s1 = sigcontext.gregs[9];
+    tf.regs.a0 = sigcontext.gregs[10];
+    tf.regs.a1 = sigcontext.gregs[11];
+    tf.regs.a2 = sigcontext.gregs[12];
+    tf.regs.a3 = sigcontext.gregs[13];
+    tf.regs.a4 = sigcontext.gregs[14];
+    tf.regs.a5 = sigcontext.gregs[15];
+    tf.regs.a6 = sigcontext.gregs[16];
+    tf.regs.a7 = sigcontext.gregs[17];
+    tf.regs.s2 = sigcontext.gregs[18];
+    tf.regs.s3 = sigcontext.gregs[19];
+    tf.regs.s4 = sigcontext.gregs[20];
+    tf.regs.s5 = sigcontext.gregs[21];
+    tf.regs.s6 = sigcontext.gregs[22];
+    tf.regs.s7 = sigcontext.gregs[23];
+    tf.regs.s8 = sigcontext.gregs[24];
+    tf.regs.s9 = sigcontext.gregs[25];
+    tf.regs.s10 = sigcontext.gregs[26];
+    tf.regs.s11 = sigcontext.gregs[27];
+    tf.regs.t3 = sigcontext.gregs[28];
+    tf.regs.t4 = sigcontext.gregs[29];
+    tf.regs.t5 = sigcontext.gregs[30];
+    tf.regs.t6 = sigcontext.gregs[31];
+}
+
+#[cfg(target_arch = "riscv64")]
+fn make_riscv_siginfo(sig: i32, code: i32, tid: i32) -> RiscvSignalInfo {
+    let mut info = RiscvSignalInfo { bytes: [0; 128] };
+    info.bytes[0..4].copy_from_slice(&sig.to_ne_bytes());
+    info.bytes[4..8].copy_from_slice(&0i32.to_ne_bytes());
+    info.bytes[8..12].copy_from_slice(&code.to_ne_bytes());
+    info.bytes[16..20].copy_from_slice(&tid.to_ne_bytes());
+    info.bytes[20..24].copy_from_slice(&0u32.to_ne_bytes());
+    info
+}
+
+#[cfg(target_arch = "riscv64")]
+pub(super) fn make_riscv_signal_frame(
+    sig: i32,
+    code: i32,
+    tid: i32,
+    current_mask: u64,
+    stack_flags: i32,
+    trampoline: [u32; 3],
+    mcontext: RiscvSignalSigcontext,
+) -> RiscvSignalFrame {
+    RiscvSignalFrame {
+        info: make_riscv_siginfo(sig, code, tid),
+        ucontext: RiscvSignalUcontext {
+            flags: 0,
+            link: 0,
+            stack: RiscvSignalStack {
+                sp: 0,
+                stack_flags,
+                stack_pad: 0,
+                size: 0,
+            },
+            sigmask: RiscvKernelSigset {
+                sig: [current_mask],
+                reserved: [0; RISCV_SIGNAL_SIGSET_RESERVED_BYTES],
+            },
+            mcontext,
+        },
+        trampoline,
+    }
 }
 
 #[cfg(target_arch = "riscv64")]
